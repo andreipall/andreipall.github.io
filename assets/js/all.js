aded3b66e4b9dd5f6ca6d37f34e730b60db0c525
Andrei Pall <andreipall@yahoo.com>
2020-01-25 10:44:51 +0200

    changed background

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,3 +1,11 @@
 jQuery(document).ready(function($){
-    // todo: jquery stuff should need to put here.
+    var pattern = Trianglify({
+    height: window.innerHeight,
+    width: window.innerWidth,
+    x_colors: ['rgba(255, 255, 255, 0.1)', 'rgba(255, 255, 255, 1)'],
+    stroke_width: 0,
+    cell_size: Math.ceil(window.innerWidth / 8)});
+    var svg = pattern.svg();
+    svg.id = 'trianglify-overlay';
+    document.body.appendChild(svg);
 });
