711be01fa33690aac9a92b15fc8784b4d20ef931
Andrei Pall <andreipall3@gmail.com>
2020-06-13 15:46:30 +0300

    Update all.js

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,11 +1,13 @@
 jQuery(document).ready(function($){
-    var pattern = Trianglify({
+  // set up the base pattern
+  const pattern = trianglify({
     height: window.innerHeight,
     width: window.innerWidth,
-    x_colors: ['rgba(255, 255, 255, 0.1)', 'rgba(255, 255, 255, 1)'],
-    stroke_width: 0,
-    cell_size: Math.ceil(window.innerWidth / 8)});
-    var svg = pattern.svg();
-    svg.id = 'trianglify-overlay';
-    document.body.appendChild(svg);
+    xColors: ['rgba(255, 255, 255, 0.1)', 'rgba(255, 255, 255, 1)'],
+    yColors: 'match',
+    cellSize: Math.ceil(window.innerWidth / 8)
+  })
+  var svg = pattern.toSVG()
+  svg.id = 'trianglify-overlay'
+  document.body.appendChild(svg)
 });
